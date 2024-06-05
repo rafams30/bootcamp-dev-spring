d544062e04f67415f4bb472ca7d42f3617d5156e
Rafael Martines <rafams_22@hotmail.com>
2024-06-04 22:08:49 -0300

    Endpoint: GET /product - V2

--- product-backend/src/main/java/com/abutua/product_backend/resources/ProductController.java
+++ product-backend/src/main/java/com/abutua/product_backend/resources/ProductController.java
@@ -1,13 +1,20 @@
 package com.abutua.product_backend.resources;
 
+import com.abutua.product_backend.models.Product;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
     @GetMapping("product")
-    public String getProduct() {
-        return "Eu sou um produto!";
+    public Product getProduct() {
+
+        Product p = new Product();
+        p.setId(1);
+        p.setName("Product 01");
+        p.setPrice(100.50);
+
+        return p;
     }
 
 }
