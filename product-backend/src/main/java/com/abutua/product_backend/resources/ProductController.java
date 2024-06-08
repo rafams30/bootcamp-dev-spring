a869941e9e47ef4e656343b29d5e276f81028055
Rafael Martines <rafams_22@hotmail.com>
2024-06-08 16:13:19 -0300

    Ajustando o Front: Tabela

--- product-backend/src/main/java/com/abutua/product_backend/resources/ProductController.java
+++ product-backend/src/main/java/com/abutua/product_backend/resources/ProductController.java
@@ -3,6 +3,7 @@ package com.abutua.product_backend.resources;
 import com.abutua.product_backend.models.Product;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class ProductController {
 
     private List<Product> products = Arrays.asList(
