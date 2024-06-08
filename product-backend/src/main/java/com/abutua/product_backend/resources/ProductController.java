b124c2257d725294455945a12e3bd0b9a80a9020
Rafael Martines <rafams_22@hotmail.com>
2024-06-08 15:20:57 -0300

    Array de Produtos

--- product-backend/src/main/java/com/abutua/product_backend/resources/ProductController.java
+++ product-backend/src/main/java/com/abutua/product_backend/resources/ProductController.java
@@ -10,35 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class ProductController {
 
-    private List<Product> products = new ArrayList<>();
-
-    @PostConstruct
-    public void init() {
-
-        Product p1 = new Product(1, "Product 01", 100.50);
-        Product p2 = new Product(2, "Product 02", 200.50);
-        Product p3 = new Product(3, "Product 03", 300.50);
-
-        products.add(p1);
-        products.add(p2);
-        products.add(p3);
-
-    }
+    private List<Product> products = Arrays.asList(
+            new Product(1, "Product 01", 100.50),
+            new Product(2, "Product 02", 200.50),
+            new Product(3, "Product 03", 300.50)
+    );
 
     @GetMapping("products/{id}")
     public ResponseEntity<Product> getProduct(@PathVariable int id) {
-
-//        if( id <= products.size()) {
-//            return ResponseEntity.ok(products.get(id-1));
-//        }
-//        else {
-//            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Product not found!");
-//        }
 
         Product prod = products.stream()
                 .filter(p -> p.getId() == id)
