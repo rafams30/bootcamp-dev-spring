99fbe2d525ee66eeef088fb7fb34229d9eab068f
Rafael Martines <rafams_22@hotmail.com>
2024-06-08 15:41:04 -0300

    Solução do desafio PRoduto

--- product-backend/src/main/java/com/abutua/product_backend/resources/ProductController.java
+++ product-backend/src/main/java/com/abutua/product_backend/resources/ProductController.java
@@ -1,7 +1,6 @@
 package com.abutua.product_backend.resources;
 
 import com.abutua.product_backend.models.Product;
-import jakarta.annotation.PostConstruct;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +15,9 @@ import java.util.List;
 public class ProductController {
 
     private List<Product> products = Arrays.asList(
-            new Product(1, "Product 01", 100.50),
-            new Product(2, "Product 02", 200.50),
-            new Product(3, "Product 03", 300.50)
+            new Product(1, "Product 01", "Description 01", 1 , false, false, 100.50),
+            new Product(2, "Product 02", "Description 02", 2 , true, true, 200.50),
+            new Product(3, "Product 03", "Description 03", 3 , false, true, 300.50)
     );
 
     @GetMapping("products/{id}")
